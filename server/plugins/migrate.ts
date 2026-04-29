198227a0c215d89054bb8b6d64111086477f799b
Tymofii Synianskyi <janosense@gmail.com>
2026-04-30 00:06:39 +0300

    Remove `migrate` plugin and update scripts for database migration with `drizzle-kit`

--- server/plugins/migrate.ts
+++
@@ -1,6 +0,0 @@
-import { migrate } from 'drizzle-orm/libsql/migrator'
-import { getDb } from '../db'
-
-export default defineNitroPlugin(async () => {
-  await migrate(getDb(), { migrationsFolder: './server/db/migrations' })
-})
