fa9dcf559a20c14feb499b328dc917bd8675da85
Tymofii Synianskyi <janosense@gmail.com>
2026-05-22 15:42:59 +0300

    Remove `Race Time` column from `LapDetailModal` for simplified table layout.

--- app/components/LapDetailModal.vue
+++ app/components/LapDetailModal.vue
@@ -8,21 +8,18 @@
               <tr>
                 <th class="text-left px-4 py-2.5 font-medium text-gray-500 dark:text-gray-400">Lap</th>
                 <th class="text-right px-4 py-2.5 font-medium text-gray-500 dark:text-gray-400">Lap Time</th>
-                <th class="text-right px-4 py-2.5 font-medium text-gray-500 dark:text-gray-400">Race Time</th>
               </tr>
             </thead>
             <tbody class="divide-y divide-gray-100 dark:divide-gray-900">
               <tr v-for="lap in participant.laps" :key="lap.lapNumber">
                 <td class="px-4 py-2.5 text-gray-500">{{ lap.lapNumber }}</td>
                 <td class="px-4 py-2.5 font-mono text-right font-medium">{{ formatMs(lap.lapTimeMs) }}</td>
-                <td class="px-4 py-2.5 font-mono text-right text-gray-400">{{ formatMs(lap.finishTimeMs) }}</td>
               </tr>
             </tbody>
             <tfoot class="bg-gray-50 dark:bg-gray-900">
               <tr>
                 <td class="px-4 py-2.5 font-medium text-gray-700 dark:text-gray-300">Total</td>
                 <td class="px-4 py-2.5 font-mono text-right font-semibold">{{ formatMs(participant.totalTimeMs) }}</td>
-                <td />
               </tr>
             </tfoot>
           </table>
